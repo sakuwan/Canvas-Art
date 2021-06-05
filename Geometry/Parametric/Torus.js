b438dc50a8fda2a6f792165451ca16c40da4ae8a
Wan <wanwebdev@gmail.com>
2021-06-04 22:12:31 -0400

    Geometry -- Torus
    Simple torus geometry, details to be implemented later

---
+++ Geometry/Parametric/Torus.js
@@ -0,0 +1,32 @@
+/** === Torus ===
+ *
+ * * Generate a torus with the provided radii and segment counts
+ *
+ * @param major    Major radius of the torus
+ * @param minor    Minor radius of the torus
+ * @param rings    Number of rings to form the torus
+ * @param segments Number of segments per ring
+*/
+
+const torusGeometry = (major, minor, rings, segments) => {
+  const ringStep = (Math.PI * 2) / rings;
+  const segmentStep = (Math.PI * 2) / segments;
+
+  const vertices = [];
+
+  for (let i = 0; i < rings; i += 1) {
+    const theta = i * ringStep;
+
+    for (let j = 0; j < segments; j += 1) {
+      const phi = j * segmentStep;
+
+      const x = (major + minor * Math.cos(phi)) * Math.cos(theta);
+      const y = (major + minor * Math.cos(phi)) * Math.sin(theta);
+      const z = minor * Math.sin(phi);
+
+      vertices.push({x, y, z});
+    }
+  }
+
+  return vertices;
+};
