485931a788f6e0ec341b5365025dd0ad8ccc5b81
Wan <wanwebdev@gmail.com>
2021-06-15 23:18:38 -0400

    Update -- GLCanvas
    More complete 2D setup for simple fragment shader prototyping

--- GLCanvas/GLCanvas.js
+++ GLCanvas/GLCanvas.js
@@ -1,7 +1,7 @@
 const noop = () => {};
 
 export class GLCanvas {
-  constructor(queryString = '', contextType = 'webgl') {
+  constructor(queryString = '', contextType = 'webgl2') {
     const canvasElement = document.querySelector(
       queryString.length ? queryString : 'canvas'
     );
@@ -10,13 +10,11 @@ export class GLCanvas {
 
     this.canvas = canvasElement;
     this.context = canvasElement.getContext(contextType);
-    if (contextType === 'webgl' && !this.context) {
-      this.context = canvasElement.getContext('experimental-webgl');
+    if (contextType === 'webgl2' && !this.context) {
+      this.context = canvasElement.getContext('webgl');
     }
 
-    this.gl = null;
-    this.vertex = null;
-    this.fragment = null;
+    this.state = {};
 
     this.lastDelta = 0;
     this.elapsedTime = 0;
@@ -28,31 +26,66 @@ export class GLCanvas {
     this.onPrerender = noop;
     this.onRender = noop;
     this.onPostrender = noop;
+
+    this.resize();
   }
 
-  initialize(vertexContent, fragmentContent, clearColor = [0, 0, 0, 1]) {
+  shader(content, type) {
     const { context } = this;
 
-    const vertexShader = context.createShader(context.VERTEX_SHADER);
-    const fragmentShader = context.createShader(context.FRAGMENT_SHADER);
+    const shaderInstance = context.createShader(type);
 
-    context.shaderSource(vertexShader, vertexContent);
-    context.shaderSource(fragmentShader, fragmentContent);
+    context.shaderSource(shaderInstance, content);
+    context.compileShader(shaderInstance);
 
-    context.compileShader(vertexShader);
-    context.compileShader(fragmentShader);
-
-    const vertexSuccess = !!context.getShaderParameter(vertexShader, context.COMPILE_STATUS);
-    if (!vertexSuccess) {
-      const shaderError = context.getShaderInfoLog(vertexShader);
+    const compileError = !context.getShaderParameter(shaderInstance, context.COMPILE_STATUS);
+    if (compileError) {
+      const shaderError = context.getShaderInfoLog(shaderInstance);
       throw new Error(`Failed to compile vertex shader: ${shaderError}`);
     }
 
-    const fragmentSuccess = !!context.getShaderParameter(fragmentShader, context.COMPILE_STATUS);
-    if (!fragmentSuccess) {
-      const shaderError = context.getShaderInfoLog(fragmentShader);
-      throw new Error(`Failed to compile fragment shader: ${shaderError}`);
-    }
+    return shaderInstance;
+  }
+
+  initialize(programOptions = {}) {
+    const { context } = this;
+
+    const glOptions = Object.assign({
+      is2D: true,
+      vertexContent: null,
+      fragmentContent: null,
+
+      clearColor: [0, 0, 0, 1],
+    }, programOptions);
+
+    let vertexContent = glOptions.is2D ? `
+      #version 300 es
+      precision highp float;
+    
+      layout (location = 0) in vec2 aPos;
+    
+      void main(void) {
+        gl_Position = vec4(aPos.xy, 0, 1);
+      }`.trim() : glOptions.vertexContent;
+
+    let fragmentContent = glOptions.is2D ? `
+      #version 300 es
+      precision highp float;
+    
+      uniform float uTime;
+      uniform vec2 uResolution;
+    
+      out vec4 outColor;
+      
+      ${glOptions.fragmentContent}
+      
+      void main(void) {
+        outColor = vec4(0, 0, 0, 1);
+        shaderMain(gl_FragCoord.xy, outColor);
+      }`.trim() : glOptions.fragmentContent;
+
+    const vertexShader = this.shader(vertexContent, context.VERTEX_SHADER);
+    const fragmentShader = this.shader(fragmentContent, context.FRAGMENT_SHADER);
 
     const glProgram = context.createProgram();
 
@@ -60,21 +93,45 @@ export class GLCanvas {
     context.attachShader(glProgram, fragmentShader);
     context.linkProgram(glProgram);
 
-    const programSuccess = !!context.getProgramParameter(glProgram, context.LINK_STATUS);
-    if (!programSuccess) {
+    const linkError = !context.getProgramParameter(glProgram, context.LINK_STATUS);
+    if (linkError) {
       const programError = context.getProgramInfoLog(glProgram);
       throw new Error(`Failed to link program: ${programError}`);
     }
 
     context.useProgram(glProgram);
 
-    context.clearColor(...clearColor);
+    context.clearColor(...glOptions.clearColor);
 
-    this.gl = glProgram;
-    this.vertex = vertexShader;
-    this.fragment = fragmentShader;
+    this.state = {
+      glProgram,
+      vertexShader,
+      fragmentShader,
 
-    this.resize();
+      uniforms: null,
+
+      is2D: glOptions.is2D,
+    };
+
+    if (glOptions.is2D) {
+      const fsTriangle = new Float32Array([-1, 3, -1, -1, 3, -1]);
+
+      const triangleBuffer = context.createBuffer();
+      context.bindBuffer(context.ARRAY_BUFFER, triangleBuffer);
+      context.bufferData(context.ARRAY_BUFFER, fsTriangle, context.STATIC_DRAW);
+
+      const posAttribute = context.getAttribLocation(glProgram, "aPos");
+      context.vertexAttribPointer(posAttribute, 2, context.FLOAT, false, 0 , 0);
+      context.enableVertexAttribArray(posAttribute);
+  
+      const timeUniform = context.getUniformLocation(glProgram, 'uTime');
+      const resUniform = context.getUniformLocation(glProgram, 'uResolution');
+  
+      this.state.uniforms = {
+        time: timeUniform,
+        resolution: resUniform,
+      };
+    }
   }
 
   resize(w = null, h = null, clear = true) {
@@ -98,6 +155,20 @@ export class GLCanvas {
 
     this.lastDelta = delta;
     this.elapsedTime += deltaTime;
+
+    if (this.state.is2D) {
+      const { canvas, context, state } = this;
+
+      const currentWidth = canvas.width;
+      const currentHeight = canvas.height;
+
+      context.viewport(0, 0, currentWidth, currentHeight);
+
+      context.uniform1f(state.uniforms.time, this.elapsedTime / 1000);
+      context.uniform2f(state.uniforms.resolution, currentWidth, currentHeight);
+    
+      context.drawArrays(context.TRIANGLES, 0, 3);
+    }
 
     this.onPrerender(deltaTime, this);
     this.onRender(deltaTime, this);
