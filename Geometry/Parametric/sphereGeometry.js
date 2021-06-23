6557cad3e42903b9efb10b699c73230bbbe06d12
Wan <wanwebdev@gmail.com>
2021-06-23 00:17:27 -0400

    Update -- Sphere
    Complete with indices, sort of overly complex for what it should be

--- Geometry/Parametric/sphereGeometry.js
+++ Geometry/Parametric/sphereGeometry.js
@@ -8,11 +8,23 @@
 */
 
 export const sphereGeometry = (radius, azimuthalSegments, polarSegments) => {
-  const indices = new Uint16Array();
+  const indexCount = (polarSegments < 3)
+    ? polarSegments * azimuthalSegments * 3
+    : (polarSegments - 1) * azimuthalSegments * 6;
+
+  const indices = new Uint16Array(indexCount);
   const vertices = new Float32Array((azimuthalSegments + 1) * (polarSegments + 1) * 3);
 
+  const sphereFaces = Array.from({ length: polarSegments + 1 },
+    (_, i) => 
+      Array.from({ length: azimuthalSegments + 1 },
+        (__, j) => i * (azimuthalSegments + 1) + j
+      )
+  );
+
   const polarStep = Math.PI / polarSegments;
-	const azimuthalStep = (Math.PI * 2) / azimuthalSegments;
+  const azimuthalStep = (Math.PI * 2) / azimuthalSegments;
+  const segmentDifference = (polarSegments + 1) + (azimuthalSegments - polarSegments);
 
   // Polar segments
   for (let i = 0; i <= polarSegments; i += 1) {
@@ -22,7 +34,7 @@ export const sphereGeometry = (radius, azimuthalSegments, polarSegments) => {
 
 		// Azimuthal segments
     for (let j = 0; j <= azimuthalSegments; j += 1) {
-      const vertexOffset = (i * polarSegments + j) * 3;
+      const vertexOffset = (i * segmentDifference + j) * 3;
 
     	const phi = j * azimuthalStep;
       const cosPhi = Math.cos(phi);
@@ -35,5 +47,26 @@ export const sphereGeometry = (radius, azimuthalSegments, polarSegments) => {
     }
   }
 
-  return vertices;
+  let indexOffset = 0;
+  for (let i = 0; i < polarSegments; i += 1) {
+    for (let j = 0; j < azimuthalSegments; j += 1) {
+      if (i > 0) {
+        indices[indexOffset + 0] = sphereFaces[i][j + 1];
+        indices[indexOffset + 1] = sphereFaces[i][j];
+        indices[indexOffset + 2] = sphereFaces[i + 1][j + 1];
+
+        indexOffset += 3;
+      }
+
+      if (i < polarSegments - 1) {
+        indices[indexOffset + 0] = sphereFaces[i][j];
+        indices[indexOffset + 1] = sphereFaces[i + 1][j];
+        indices[indexOffset + 2] = sphereFaces[i + 1][j + 1];
+
+        indexOffset += 3;
+      }
+    }
+  }
+
+  return { vertices, indices };
 };
