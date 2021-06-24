02bcb8cebe52eb5f8517f08eff64d45bbd281886
Wan <wanwebdev@gmail.com>
2021-06-24 00:58:10 -0400

    Update -- Torus
    Full vertices & indices for torus geometries

--- Geometry/Parametric/torusGeometry.js
+++ Geometry/Parametric/torusGeometry.js
@@ -9,24 +9,42 @@
 */
 
 export const torusGeometry = (major, minor, rings, segments) => {
+  const indices = new Uint16Array(rings * segments * 6);
+  const vertices = new Float32Array((rings + 1) * (segments + 1) * 3);
+
   const ringStep = (Math.PI * 2) / rings;
   const segmentStep = (Math.PI * 2) / segments;
+  const segmentDifference = (rings + 1) + (segments - rings);
 
-  const vertices = [];
-
-  for (let i = 0; i < rings; i += 1) {
+  for (let i = 0; i <= rings; i += 1) {
     const theta = i * ringStep;
+    const cosTheta = Math.cos(theta);
+    const sinTheta = Math.sin(theta);
 
-    for (let j = 0; j < segments; j += 1) {
+    for (let j = 0; j <= segments; j += 1) {
+      const vertexOffset = (i * segmentDifference + j) * 3;
+
       const phi = j * segmentStep;
 
-      const x = (major + minor * Math.cos(phi)) * Math.cos(theta);
-      const y = (major + minor * Math.cos(phi)) * Math.sin(theta);
-      const z = minor * Math.sin(phi);
-
-      vertices.push({x, y, z});
+      vertices[vertexOffset + 0] = (major + minor * cosTheta) * Math.cos(phi);
+      vertices[vertexOffset + 1] = (major + minor * cosTheta) * Math.sin(phi);
+      vertices[vertexOffset + 2] = minor * sinTheta;
     }
   }
 
-  return vertices;
+  for (let i = 1; i <= rings; i += 1) {
+    for (let j = 1; j <= segments; j += 1) {
+      const indexOffset = ((i - 1) * segments + (j - 1)) * 6;
+
+      indices[indexOffset + 0] = (segments + 1) * i + j - 1;
+      indices[indexOffset + 1] = (segments + 1) * (i - 1) + j - 1;
+      indices[indexOffset + 2] = (segments + 1) * i + j;
+
+      indices[indexOffset + 3] = (segments + 1) * (i - 1) + j - 1;
+      indices[indexOffset + 4] = (segments + 1) * (i - 1) + j;
+      indices[indexOffset + 5] = (segments + 1) * i + j;
+    }
+  }
+
+  return { vertices, indices };
 };
