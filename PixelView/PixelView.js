d80a360f4c3beee714834ca9e352c15af8683062
Wan <wanwebdev@gmail.com>
2021-07-04 17:59:10 -0400

    Update -- PixelView
    Utility class, some minor clean-up.

--- PixelView/PixelView.js
+++ PixelView/PixelView.js
@@ -1,31 +1,32 @@
 class PixelView {
   constructor(canvasWidth = null, canvasHeight = null) {
-    const width = canvasWidth || window.innerWidth;
-    const height = canvasHeight || window.innerHeight;
+    const width = canvasWidth ?? window.innerWidth;
+    const height = canvasHeight ?? window.innerHeight;
 
-    const canvasElement = (typeof OffscreenCanvas === undefined)
-      ? createDOMCanvas(width, height)
-      : new OffscreenCanvas(width, height);
-
+    const canvasElement = this.createCanvas(width, height);
     const canvasContext = canvasElement.getContext('2d', {
       desynchronized: true,
     });
 
     const imageData = canvasContext.createImageData(width, height);
+
     const pixelData = new DataView(imageData.data.buffer);
     const uint32Data = new Uint32Array(imageData.data.buffer);
 
     this.canvas = canvasElement;
     this.context = canvasContext;
-    
+
     this.imageData = imageData;
     this.pixelData = pixelData;
     this.uint32Data = uint32Data;
   }
 
+  getPixel(x, y) {
+    return this.pixelData.getUint32(this.fetchIndex(x, y));
+  }
+
   setPixel(x, y, color) {
-    const pixelOffset = (x + y * this.imageData.width) * 4;
-    this.pixelData.setUint32(pixelOffset, color);
+    this.pixelData.setUint32(this.fetchIndex(x, y), color);
 
     return this;
   }
@@ -36,7 +37,20 @@ class PixelView {
     return this;
   }
 
-  createDOMCanvas(width, height) {
+  fetchIndex(x, y) {
+    const { width, height } = this.imageData;
+    if ((x >= width || x < 0) || (y >= height || y < 0)) {
+      throw RangeError(`[PixelView::fetchIndex] Index (${x}, ${y}) out of bounds`);
+    }
+
+    return (x + y * this.imageData.width) * 4;
+  }
+
+  createCanvas(width, height) {
+    if (typeof OffscreenCanvas !== undefined) {
+      return new OffscreenCanvas(width, height);
+    }
+
     const canvasElement = document.createElement('canvas');
 
     canvasElement.width = width;
