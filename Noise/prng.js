5e24546ce242ca71aae279d06f33049ae4b208b9
Wan <wanwebdev@gmail.com>
2021-07-08 15:45:42 -0400

    Noise -- PRNG
    Fast, but efficient, mulberry prng for future perlin & simplex noise

---
+++ Noise/prng.js
@@ -0,0 +1,23 @@
+
+/**
+ * @function mulberry32
+ * @description Create a mulberry32 PRNG instance
+ *
+ * @param {uint32} startSeed - Initial seed
+ * @returns {float} - A single [0..1] normalized float
+ *
+ * @see https://gist.github.com/tommyettinger/46a874533244883189143505d203312c
+ */
+const mulberry32 = (startSeed) => {
+  const mulberryConstant = 0x6D2B79F5 | 0;
+
+  let seed = startSeed | 0;
+  return () => {
+   	seed += mulberryConstant;
+
+    const bitsA = Math.imul(seed ^ seed >>> 15, seed | 1);
+    const bitsB = bitsA + Math.imul(bitsA ^ bitsA >>> 7, bitsA | 61) ^ bitsA;
+
+    return ((bitsB ^ bitsB >>> 14) >>> 0) * 2.3283064365386963e-10;
+  }
+}
