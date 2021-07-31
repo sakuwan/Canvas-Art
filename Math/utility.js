c8641a10d64cce928a5c31f61bee9c8bb995cac6
Wan <wanwebdev@gmail.com>
2021-07-31 02:35:37 -0400

    Update -- Math utils
    Some fixing since I had initially wrote this without any linting

--- Math/utility.js
+++ Math/utility.js
@@ -3,16 +3,17 @@ export const HALF_PI = Math.PI * 0.5;
 export const PHI = (1 + Math.sqrt(5)) * 0.5;
 
 export const round = (value, precision = 0) => {
-    const precisionScale = Math.pow(10, precision);
-    return Math.round(value * precisionScale) / precisionScale;
+  const precisionScale = 10 ** precision;
+
+  return Math.round(value * precisionScale) / precisionScale;
 };
 
-export const clamp = (value, min = 0, max = 1) => {
-  return Math.min(Math.max(value, Math.min(min, max)), Math.max(min, max));
-}
+export const clamp = (value, min = 0, max = 1) => (
+  Math.min(Math.max(value, Math.min(min, max)), Math.max(min, max))
+);
 
 export const scale = (value, fromMin, fromMax, toMin, toMax, isClamped = false) => {
-  const scaledValue = (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
+  const scaledValue = (value - fromMin) * ((toMax - toMin) / (fromMax - fromMin)) + toMin;
 
   return isClamped
     ? clamp(scaledValue, Math.min(toMin, toMax), Math.max(toMin, toMax))
@@ -20,22 +21,21 @@ export const scale = (value, fromMin, fromMax, toMin, toMax, isClamped = false) 
 };
 
 export const lerp = (a, b, alpha) => (1 - alpha) * a + b * alpha;
-export const invlerp = (a, b, alpha) => a !== b ? (alpha - a) / (b - a) : 0;
+export const invlerp = (a, b, alpha) => (a !== b ? (alpha - a) / (b - a) : 0);
 
-export const step = (edge, value) => value < edge ? 0 : 1;
+export const step = (edge, value) => (value < edge ? 0 : 1);
 export const smoothStep = (min, max, value) => {
   const x = Math.max(0, Math.min(1, (value - min) / (max - min)));
 
   return x * x * (3 - 2 * x);
-}
-
+};
 export const smootherStep = (min, max, value) => {
   const x = Math.max(0, Math.min(1, (value - min) / (max - min)));
 
-	return x * x * x * (x * (x * 6 - 15) + 10);
-}
+  return x * x * x * (x * (x * 6 - 15) + 10);
+};
 
-export const mod = (value, n) => (value % n + n) % n;
+export const mod = (value, n) => ((value % n) + n) % n;
 export const fract = (value) => value - Math.floor(value);
 
 export const rand = (min, max) => {
